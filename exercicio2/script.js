a185ffb1942a545dff1b174fbed8bd6d8b6bfed7
Rafael Machado <machadorafael27@gmail.com>
2023-05-11 20:31:44 -0300

    exercicios revisão

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,6 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let novaFrase = minhaString.trim()
+let substiuicao = novaFrase.replaceAll("________", "sticioso");
+console.log(minhaString.length)
+console.log(novaFrase.length);
+console.log(substiuicao);
